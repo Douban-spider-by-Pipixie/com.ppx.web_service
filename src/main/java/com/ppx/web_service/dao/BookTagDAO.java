9572476a5cc9c4c9feb2651f729ed23e3c2117d7
Minghou Lei <36237320+Mistletoer@users.noreply.github.com>
2020-05-14 15:51:48 +0800

    DAO Structure made
    Signed-off-by: Minghou Lei <36237320+Mistletoer@users.noreply.github.com>

---
+++ src/main/java/com/ppx/web_service/dao/BookTagDAO.java
@@ -0,0 +1,29 @@
+package com.ppx.web_service.dao;
+
+import org.hibernate.query.Query;
+
+public class BookTagDAO extends BasicDAO {
+	public BookTagDAO() {
+		super();
+	}
+
+	@Override
+	public boolean insert(Object o) {
+		return super.insert(o);
+	}
+
+	@Override
+	public boolean delete(Object o) {
+		return super.delete(o);
+	}
+
+	@Override
+	public boolean update(Object o) {
+		return super.update(o);
+	}
+
+	@Override
+	Query getQueryByHQL(String hql) {
+		return super.getQueryByHQL(hql);
+	}
+}
